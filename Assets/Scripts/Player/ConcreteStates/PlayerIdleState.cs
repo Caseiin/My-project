afde926617ed3d0574d3c001eb721f5cf89de22a
Caseiin <leruopono@gmail.com>
2026-03-04 19:06:03 +0200

    Player/ConcreteStates| Player state machine works and has seemless transitions between states

--- Assets/Scripts/Player/ConcreteStates/PlayerIdleState.cs
+++ Assets/Scripts/Player/ConcreteStates/PlayerIdleState.cs
@@ -7,5 +7,9 @@ public class PlayerIdleState : BaseState
     {
     }
 
-    public override void OnEnter() => Debug.Log("Player is Idling!");
+    public override void OnEnter()
+    {
+        Debug.Log("Player is Idling!");
+        // _player.RB.linearVelocity = Vector2.zero;
+    }
 }
--- Assets/Scripts/Player/ConcreteStates/PlayerMotionState.cs
+++ Assets/Scripts/Player/ConcreteStates/PlayerMotionState.cs
@@ -1,36 +1,36 @@
 using UnityEngine;
 
 public class PlayerMotionState : BaseState
-{
-    
-    private PlayerController player;
+{  
+    private PlayerController _player;
 
     public PlayerMotionState(PlayerController player) : base(player)
     {
-        this.player = player;
+        _player = player;
     }
 
     public override void OnEnter() => Debug.Log("Player is moving!");
+    public override void OnExit() => _player.RB.linearVelocity = Vector2.zero;
 
 
     public override void FixedUpdate()
     {
         Move();
 
-        if (player.RB.linearVelocity.y < 0)
+        if (_player.RB.linearVelocity.y < 0)
         {
-            player.RB.linearVelocity += Vector3.up * Physics.gravity.y * (player.FallMultiplier - 1) * Time.fixedDeltaTime;
+            _player.RB.linearVelocity += Vector3.up * Physics.gravity.y * (_player.FallMultiplier - 1) * Time.fixedDeltaTime;
         }
     }
 
     void Move()
     {
-        var move = new Vector3(player.Input.MoveDirection.x, 0f, player.Input.MoveDirection.y);
-        player.RB.linearVelocity = new Vector3(move.x * player.MoveSpeed,player.RB.linearVelocity.y,move.z * player.MoveSpeed);
+        var move = new Vector3(_player.Input.MoveDirection.x, 0f, _player.Input.MoveDirection.y);
+        _player.RB.linearVelocity = new Vector3(move.x * _player.MoveSpeed,_player.RB.linearVelocity.y,move.z * _player.MoveSpeed);
     }
 
     void Jump()
     {
-        player.RB.linearVelocity= new Vector3(player.RB.linearVelocity.x, player.JumpForce, player.RB.linearVelocity.z);
+        _player.RB.linearVelocity= new Vector3(_player.RB.linearVelocity.x, _player.JumpForce, _player.RB.linearVelocity.z);
     }
 }
