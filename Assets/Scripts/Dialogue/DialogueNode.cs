d5d760f9f524536a307aa4dc2483dbf37a22f39d
Caseiin <leruopono@gmail.com>
2026-03-06 16:23:03 +0200

    Dialogue| SO Data addded

--- Assets/Scripts/Dialogue/DialogueNode.cs
+++ Assets/Scripts/Dialogue/DialogueNode.cs
@@ -3,5 +3,5 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "DialogueNode", menuName = "Scriptable Objects/DialogueNode")]
 public class DialogueNode : ScriptableObject
 {
-    
+    public DialogueOptions [] Dialogues;
 }
