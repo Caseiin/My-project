ce3f58c4b5e5999912d53b93de8339f2c0c7087c
Caseiin <leruopono@gmail.com>
2026-03-06 16:13:02 +0200

    Singleton| Added PersistentSingleton

---
+++ Assets/Scripts/Singleton/PersistentSingleton.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class PersistentSingleton<T> : MonoBehaviour where T : Component
+{
+    protected static T _instance;
+    public static bool HasInstance => _instance != null;
+    public static T TryGetInstance() => HasInstance? _instance : null;
+
+    public static T Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = FindAnyObjectByType<T>();
+                if(_instance == null)
+                {
+                    var _gameObject = new GameObject(typeof(T).Name+ "Auto-Generated");
+                    _instance = _gameObject.AddComponent<T>();
+                }
+            }
+
+            return _instance;
+        }
+    }
+
+    protected virtual void Awake()
+    {
+        InitialiseSingleton();
+    }
+// Add persistent logic
+    protected virtual void InitialiseSingleton()
+    {
+        if (!Application.isPlaying) return;
+
+        _instance = this as T;
+    }
+}
