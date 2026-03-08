dffb1334d4df4e4e8e35cba76e5523e1e4edfb5b
Caseiin <leruopono@gmail.com>
2026-03-08 13:39:24 +0200

    AbilityDispenser.cs| works

---
+++ Assets/Scripts/Abilities/Testing/Ability Dispenser.cs
@@ -0,0 +1,54 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AbilityDispenser : MonoBehaviour
+{
+    [SerializeField] List<AbilitySO> _abilityList;
+    [SerializeField] GameObject _abilityObject;
+    [SerializeField] float spawnRadius = 5f;
+    [SerializeField] float minSpawnRadius = 2f;
+    [SerializeField] float maxSpawnRadius = 5f;
+
+
+    GameObject AbilityFactory(AbilitySO ability)
+    {
+        var abilityInstance = Instantiate(_abilityObject);
+        var tester = _abilityObject.GetComponent<Tester>();
+        tester.Ability = ability;
+        return abilityInstance;
+    }
+
+    void PlaceAbilityTrip(GameObject ability)
+    {
+        Vector2 direction = Random.insideUnitCircle.normalized;
+        float distance = Random.Range(minSpawnRadius, maxSpawnRadius);
+
+        Vector3 spawnOffset = new Vector3(direction.x, 0, direction.y) * distance;
+        Vector3 spawnPosition = transform.position + spawnOffset;
+
+        ability.transform.position = spawnPosition;
+    }
+
+    void DispenseAbility()
+    {
+        if (_abilityList.Count == 0) return;
+        var ability = _abilityList[Random.Range(0, _abilityList.Count)];
+        var abilityObject = AbilityFactory(ability);
+        PlaceAbilityTrip(abilityObject);
+    }
+
+    void Start()
+    {
+        InvokeRepeating(nameof(DispenseAbility),0f,5f);
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, maxSpawnRadius);
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, minSpawnRadius);
+    }
+
+}
--- Assets/Scripts/Abilities/Testing/Tester.cs
+++ Assets/Scripts/Abilities/Testing/Tester.cs
@@ -1,19 +1,21 @@
+using Mono.Cecil.Cil;
 using UnityEngine;
 
 public class Tester : MonoBehaviour
 {
-    [SerializeField] AbilitySO ability;
+    public AbilitySO Ability;
     void OnTriggerEnter(Collider other)
     {
         var effectables = other.gameObject.GetComponents<IEffectable>();
         foreach(var effectable in effectables)
         {
             Debug.Log($"effectable components: {effectable}");
-            foreach ( var effect in ability.effects)
+            foreach ( var effect in Ability.effects)
             {
                 Debug.Log($"effects: {effect}");
                 effect.Apply(effectable);
             }
         }
     }
+
 }
