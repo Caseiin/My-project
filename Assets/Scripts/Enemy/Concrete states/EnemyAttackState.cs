b0c6e4b35950e988f889b17e214fa7d3b1da84c3
Caseiin <leruopono@gmail.com>
2026-03-10 18:50:05 +0200

    EnemyAttackState.cs| Added enemy attacking
    This is a temporary solution

--- Assets/Scripts/Enemy/Concrete states/EnemyAttackState.cs
+++ Assets/Scripts/Enemy/Concrete states/EnemyAttackState.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class EnemyAttackState : BaseState
 {
     EnemyController _enemy;
+    PlayerHealth _playerHealth;
 
     public EnemyAttackState(EnemyController enemy) : base(enemy)
     {
@@ -12,5 +13,9 @@ public class EnemyAttackState : BaseState
     public override void OnEnter()
     {
         Debug.Log("Enemy is attacking");
+        _playerHealth = Object.FindFirstObjectByType<PlayerHealth>();
+        _playerHealth.TakeDamage(30);
+
+        Debug.Log("Enemy hit player for 30 dmg");
     }
 }
