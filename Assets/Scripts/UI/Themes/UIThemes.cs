6a6723c979818eeae1a48c9673977ad3c7d87b8d
Caseiin <leruopono@gmail.com>
2026-03-20 12:58:20 +0200

    UI| ThemedUI and ThemeUI SO setup

---
+++ Assets/Scripts/UI/Themes/UIThemes.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "UIThemes", menuName = "UI/Themes")]
+public class UIThemes : ScriptableObject
+{
+    public Color primaryColour;
+    public Color secondaryColour;
+}
