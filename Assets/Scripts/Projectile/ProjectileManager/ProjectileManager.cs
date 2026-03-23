55b1a8db02b79b47442b73ca890c433e2b1e93d8
Caseiin <leruopono@gmail.com>
2026-03-23 23:31:06 +0200

    ProjectileManager| ObjectPool initial Setup
    Objectbool has not been tested

---
+++ Assets/Scripts/Projectile/ProjectileManager/ProjectileManager.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using UnityEngine.Pool;
+
+public class ProjectileManager : Singleton<ProjectileManager>
+{
+    [SerializeField] Transform _initialPosition;
+    [SerializeField] GameObject projectilePrefab;
+    ObjectPool<AbilityProjectile> _pool;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        _pool = new ObjectPool<AbilityProjectile>(
+            CreateProjectile,
+            OnGetFromPool,
+            OnReleaseToPool,
+            OnDestroyPoolObject,
+            collectionCheck: false,
+            defaultCapacity: 5,
+            maxSize: 10
+        );
+    }
+
+
+    AbilityProjectile CreateProjectile()
+    {
+        GameObject projectileObj = Instantiate(projectilePrefab,_initialPosition);
+        projectileObj.SetActive(false);
+        return projectileObj.GetComponent<AbilityProjectile>();
+    }
+
+    private void OnGetFromPool(AbilityProjectile abilityProjectile)
+    {
+        abilityProjectile.gameObject.SetActive(true);
+    }
+
+    private void OnReleaseToPool(AbilityProjectile abilityProjectile)
+    {
+        abilityProjectile.gameObject.SetActive(false);
+    }
+
+    private void OnDestroyPoolObject(AbilityProjectile abilityProjectile)
+    {
+        Destroy(abilityProjectile.gameObject);
+    }
+}
