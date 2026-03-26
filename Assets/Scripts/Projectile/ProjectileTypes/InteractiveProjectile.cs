6e33ed71cc5eedc0cc746e4bde8ab6109c22a566
Caseiin <leruopono@gmail.com>
2026-03-26 20:09:11 +0200

    InteractiveProjectile.cs| Removed Sprite

--- Assets/Scripts/Projectile/ProjectileTypes/InteractiveProjectile.cs
+++ Assets/Scripts/Projectile/ProjectileTypes/InteractiveProjectile.cs
@@ -3,17 +3,17 @@ using UnityEngine;
 
 public class InteractiveProjectile : AbilityProjectile
 {
-    public Sprite InteractIcon;
     public InteractiveProjectileUI interactivePrefab;
+    
     protected override void Awake()
     {
         base.Awake();
+
     }
     public void ToInteract()
     {
         Activate();
     }
+    
     public override void Launch(Vector3 direction){}
-
-
 }
