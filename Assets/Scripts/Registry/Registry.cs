ee4fa391dbf00824a03574858519bc0c44191098
Caseiin <leruopono@gmail.com>
2026-03-30 21:04:29 +0200

    Registry Creation

---
+++ Assets/Scripts/Registry/Registry.cs
@@ -0,0 +1,25 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public delegate T SelectionStrategy<T>(IEnumerable<T> items); 
+public delegate IEnumerable<T> FilterStrategy<T>(IEnumerable<T> items); 
+
+
+public static class Registry<T> where T : class
+{
+
+    static readonly HashSet<T> items = new();
+
+    public static bool TryAdd(T item)
+    {
+        return item != null & items.Add(item);
+    }
+
+    public static bool Remove(T item)
+    {
+        return items.Remove(item);
+    }
+
+    public static T Get(SelectionStrategy<T> strategy) => strategy(items);
+    public static IEnumerable<T> GetMany(FilterStrategy<T> filter) => filter(items);
+}
