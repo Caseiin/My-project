e638f0c85a8e22d8c550f003013d78956f765947
Caseiin <leruopono@gmail.com>
2026-03-31 21:12:47 +0200

    InteractiveProjectileUI.cs| removal

--- Assets/Scripts/UI/WorldSpace/WorlspaceTypes/InteractiveProjectileUI.cs
+++ Assets/Scripts/UI/WorldSpace/WorlspaceTypes/InteractiveProjectileUI.cs
@@ -6,8 +6,7 @@ public class InteractiveProjectileUI : WorldSpaceUIFollower
     [SerializeField] string _interactMessage;
     [SerializeField] TextMeshProUGUI _text;
     Vector3 _originalScale;
-    Vector3 _newOffset = new Vector3(0,.1f,-1);
-
+    Vector3 _newOffset = new Vector3(0, 0.1f, -1f); // push 1 unit toward the camera
     void Awake()
     {
         _originalScale = transform.localScale;
@@ -24,7 +23,8 @@ public class InteractiveProjectileUI : WorldSpaceUIFollower
 
     protected override void UpdatePosition()
     {
-        transform.position = _target.position + _newOffset;
+        Vector3 worldOffset = _camera.transform.TransformDirection(_newOffset);
+        transform.position = _target.position + worldOffset;
     }
     public void Hide()
     {
