c5e14d48a4925099c1173eb4995273dd433f6e8a
Caseiin <leruopono@gmail.com>
2026-04-02 16:55:54 +0200

    Sound| Emitter & Manager added functionality: stop,play and pool

--- Assets/Scripts/Sound/SoundEmitter.cs
+++ Assets/Scripts/Sound/SoundEmitter.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections;
 using UnityEngine;
 
 [RequireComponent(typeof(AudioSource))]
@@ -6,8 +8,44 @@ public class SoundEmitter : MonoBehaviour
     AudioSource _audioSource;
     Coroutine _playingCoroutine;
 
-    public void InitializeSound(SoundData data){}
-    public void Play(){}
-    public void Stop(){}
+    void Awake()
+    {
+        _audioSource = GetComponent<AudioSource>();
+    }
+
+    public void InitializeSound(SoundData data)
+    {
+        _audioSource.clip = data.Clip;
+        _audioSource.outputAudioMixerGroup = data.mixerGroup;
+        _audioSource.loop = data.Loop;
+        _audioSource.playOnAwake = data.PlayonAwake;
+    }
+    public void Play()
+    {
+        if (_playingCoroutine != null){
+            StopCoroutine(_playingCoroutine);
+        }
+
+        _audioSource.Play();
+        StartCoroutine(WaitForSoundEnd());
+    }
+
+    private IEnumerator WaitForSoundEnd()
+    {
+        yield return new WaitWhile(()=> _audioSource.isPlaying);
+        SoundManager.Instance.ReturnToPool(this);
+    }
+
+    public void Stop()
+    {
+        if (_playingCoroutine != null){
+            StopCoroutine(_playingCoroutine);
+            _playingCoroutine = null;
+        }
+
+        _audioSource.Stop();
+        SoundManager.Instance.ReturnToPool(this);
+    }
+
 
 }
--- Assets/Scripts/Sound/SoundManager.cs
+++ Assets/Scripts/Sound/SoundManager.cs
@@ -1,13 +1,77 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Pool;
 
 public class SoundManager : PersistentSingleton<SoundManager>
 {
-    IObjectPool<SoundEmitter> _soundPool;
+    IObjectPool<SoundEmitter> _soundEmitterPool;
+    readonly List<SoundEmitter> _activeSoundEmitters = new();
+    public readonly Dictionary<SoundData, int> Counts = new();
 
-    protected override void Awake()
+ 
+    [SerializeField] SoundEmitter soundEmitterPrefab;
+    [SerializeField] bool collectionCheck = true;
+    [SerializeField] int defaultSoundCapacity = 10;
+    [SerializeField] int maxSoundCapacity = 30;
+
+    void Start()
     {
-        base.Awake();
+        InitializePool();
+    }
+
+    public SoundEmitter Get()
+    {
+        return _soundEmitterPool.Get();
+    }
+
+    public void ReturnToPool(SoundEmitter emitter)
+    {
+        _soundEmitterPool.Release(emitter);
+    }
+
+    private void InitializePool()
+    {
+        _soundEmitterPool = new ObjectPool<SoundEmitter>(CreateSoundEmitter, OnTakefromPool, OnReturnToPool, OnDestroyFromPool,collectionCheck,defaultSoundCapacity,maxSoundCapacity
+);
+    }
+
+    private void OnDestroyFromPool(SoundEmitter emitter)
+    {
+        Destroy(emitter.gameObject);
+    }
+
+    private void OnReturnToPool(SoundEmitter emitter)
+    {
+        emitter.gameObject.SetActive(false);
+        _activeSoundEmitters.Remove(emitter);
+    }
+
+    private void OnTakefromPool(SoundEmitter emitter)
+    {
+        emitter.gameObject.SetActive(true);
+        _activeSoundEmitters.Add(emitter);
+    }
+
+    private SoundEmitter CreateSoundEmitter()
+    {
+        var soundEmitter = Instantiate(soundEmitterPrefab);
+        soundEmitter.gameObject.SetActive(false);
+        return soundEmitter;
+    }
+
+
+    // Helper method for SoundData count managment
+    public bool CanPlaySound(SoundData data)
+    {
+        if (Counts.TryGetValue(data, out var count))
+        {
+            if (count >= maxSoundCapacity)
+            {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
