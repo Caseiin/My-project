c037ea622feca288b7e2697947609890eb7c0f87
Caseiin <leruopono@gmail.com>
2026-05-19 17:33:26 +0200

    ProjectileThrow.cs| Throw limitation works
    However, I do not like how its coded. I may refactor it to be more readable

--- Assets/Scripts/Projectile/ProjectileThrow.cs
+++ Assets/Scripts/Projectile/ProjectileThrow.cs
@@ -14,16 +14,25 @@ public class ProjectileThrow : MonoBehaviour
     Transform throwPoint;
     AbilitySO[] _selectedAbility;
     int currentIndex = 0;
+    bool CanThrow = true;
+    int _currentCount = 0;
+
+    CountdownTimer _countdown;
 
     void Awake()
     {
         throwPoint = transform;
-
+        _countdown = new CountdownTimer(_cooldownDuration);
         // initialise with projectiles's default Ability
         _selectedAbility = new AbilitySO[projectiles.Count];
         for(int i = 0; i < projectiles.Count; i++){
             _selectedAbility[i] = projectiles[i].ability;
         }
+    }
+
+    void Update()
+    {
+        _countdown.Tick(Time.deltaTime);
     }
 
     public Vector3 CalculateThrowVelocity()
@@ -47,20 +56,25 @@ public class ProjectileThrow : MonoBehaviour
     public void Throw()
     {
         // TODO: Add logic to limit throwing whether that be a cooldown or a throw amount limit
+        if(!CanThrow || _currentCount >= _maxCount) return;
         
         AbilityProjectile proj = ProjectileManager.Instance.GetProjectile(projectiles[currentIndex]);
+        _currentCount ++;
 
         if (_selectedAbility != null)
             proj.SetAbility(_selectedAbility[currentIndex]);
 
         
         Rigidbody rb = proj.GetComponent<Rigidbody>();
-        
         proj.transform.SetPositionAndRotation(throwPoint.position, Quaternion.identity);
 
         Vector3 dir = Camera.main.transform.forward;
         Vector3 impulse = dir * throwForce + Vector3.up * upwardForce;
-        proj.Launch(impulse); 
+        proj.Launch(impulse);
+
+        if(_currentCount >= _maxCount){
+            StartThrowCoolDown();
+        }
     }
 
     void SetProjectileAbility(AbilitySO ability) => _selectedAbility[currentIndex] = ability;
@@ -69,5 +83,18 @@ public class ProjectileThrow : MonoBehaviour
         string msg = (newIndex > 0)? "Impact Bomb": "Interactive Bomb";
         Debug.Log($"Equipped: {msg}");
         Messenger.AddEquipMessage(msg);
+    }
+
+    void ResetThrow(){
+        CanThrow = true;
+        _currentCount = 0;
+        Debug.Log("Throw CoolDown ended!");
+    }
+
+    void StartThrowCoolDown(){
+        _countdown.OnTimerStart = ()=> CanThrow = false;
+        _countdown.OnTimerStop = ()=> ResetThrow();
+        _countdown.Start();
+        Debug.Log("Throw CoolDown started!");
     }
 }
